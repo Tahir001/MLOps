b0e19bb128bf4a99fb9d26b322da743e52995286
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2025-04-26 00:04:52 -0400

    Update: Python work

---
+++ Python_Complete_Materials/5-Modules/mypackage/__init__.py
@@ -0,0 +1,4 @@
+# Inside mypackage/__init__.py
+from .module1 import add
+from .module2 import multiply
+from .subpackage.module3 import divide
---
+++ Python_Complete_Materials/5-Modules/mypackage/subpackage/__init__.py
@@ -0,0 +1,2 @@
+# inside mypackage/subpackage/__init__.py
+from.module3 import divide
